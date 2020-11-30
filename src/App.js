542d20bab1eac215768892519dc6535a39060371
mdjek <32070769+mdjek@users.noreply.github.com>
2020-11-30 15:42:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,8 +24,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <div>Variant: {`${variant}`}</div>
+    <div className="App">      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
