5a0c296206e46805aedc93350bb32e62de8964a1
mdjek <32070769+mdjek@users.noreply.github.com>
2020-11-30 15:26:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [variant, setVariant] = useState(0);
+  const [variant, setVariant] = useState('0');
 
   useEffect(() => {
     async function abTest() {
