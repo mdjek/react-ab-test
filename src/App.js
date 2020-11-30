823d46851a30087883211dd363a6f58dc9646867
mdjek <32070769+mdjek@users.noreply.github.com>
2020-11-30 11:23:01 +0300

    added ab test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [variant, setVariant] = useState(0);
+  
+  useEffect(() => {
+    (async function abTest() {      
+      if (window.dataLayer) {
+        await window.dataLayer.push({ event: 'optimize.activate' });
+      }
+      this.intervalId = setInterval(() => {
+        if (window.google_optimize !== undefined) {
+          const variant = window.google_optimize.get('o30slX_MQROZiJYFguu9Mg');
+          this.setState({ variant });
+          clearInterval(this.intervalId);
+        }
+    }, 100);
+    })();
+  });
+  
   return (
     <div className="App">
-      <header className="App-header">
+      <div>Variant: {`${variant}`}</div>
+      <header className="App-header">        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
